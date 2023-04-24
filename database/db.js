d8e5b2293d3db235304a94e15fbad1ac334127c0
99pmore <morenomartin.pablo@gmail.com>
2023-04-24 12:49:00 +0200

    commit

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-// mongoose.connect('mongodb://localhost:27017/goodtravels')
-mongoose.connect('mongodb+srv://pmore:mon916334082@cluster0.r5x9zeg.mongodb.net/goodtravels')
+mongoose.connect('mongodb://localhost:27017/goodtravels')
+// mongoose.connect('mongodb+srv://pmore:mon916334082@cluster0.r5x9zeg.mongodb.net/goodtravels')
 .then(db => console.log('db connect'))
 .catch(err => console.log('Error: ', err))
