21a7ffd240c828f1b240fd2d2d2a8126191a006f
99pmore <morenomartin.pablo@gmail.com>
2023-04-24 13:22:18 +0200

    atlas url

--- database/db.js
+++ database/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/goodtravels')
+// mongoose.connect('mongodb://localhost:27017/goodtravels')
+mongoose.connect('mongodb+srv://pmore:mon916334082@cluster0.r5x9zeg.mongodb.net/goodreads')
 .then(db => console.log('db connect'))
 .catch(err => console.log('Error: ', err))
