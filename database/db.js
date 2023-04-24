330787b600d434861edbeab4f8017ba092b12e8c
99pmore <morenomartin.pablo@gmail.com>
2023-04-24 13:38:16 +0200

    mongo url

--- database/db.js
+++ database/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 // mongoose.connect('mongodb://localhost:27017/goodtravels')
-mongoose.connect(process.env.MONGODB_URL)
+// mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect('mongodb+srv://pmore:mongogoodtravels@cluster0.r5x9zeg.mongodb.net/goodtravels')
 .then(db => console.log('db connect'))
 .catch(err => console.log('Error: ', err))
